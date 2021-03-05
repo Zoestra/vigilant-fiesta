c5ef8b7fc9670e3cc6eb5a2cd7826f8cf636dde7
Zoestra <79121105+Zoestra@users.noreply.github.com>
2021-03-05 08:21:09 -0800

    Create connect 4.py

---
+++ other projects/connect 4.py
@@ -0,0 +1,29 @@
+'''
+Connect 4
+
+ask for player names
+load blank board
+accept player inputs for column placement
+place counter in lowest position in column
+check for win state vertically, horizontally, and diagonally
+if win, print boardstate to file.
+
+what is needed:
+
+boardstate array
+winstate check functions
+
+'''
+
+def create_board:
+    board = for i 
+
+
+
+def check_win:
+
+
+winstate = check_win(board)
+def main:
+
+while winstate == False:
